92b792f953b6fc228af79451c52c1f14037f04c4
Adam.Kazberuk <hodrobond@yahoo.com>
2016-12-07 09:21:58 -0800

    learned about a shorthand for components

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -3,18 +3,8 @@
  */
 import React, { Component, PropTypes } from 'react'
 
-class Tile extends Component{
-    static propTypes = {
-        number: PropTypes.number
-    }
-    render(){
-        const {number} = this.props;
-        return (
-            <td>
-                {this.props.number}
-            </td>
-        )
-    }
-}
+const Tile = props => (
+    <td>{props.number}</td>
+)
 
 export default Tile
