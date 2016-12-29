117529f05f87f92abd268e9b21c51a2aba7a7313
Adam Kazberuk <hodrobond@Adams-MBP.hsd1.ca.comcast.net>
2016-12-29 07:30:26 -0800

    hopefully github pages

--- src/actions/History.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by adam.kazberuk on 12/15/2016.
- */
-import { ActionCreators as UndoActionCreators } from 'redux-undo'
-
-export const undo = () => UndoActionCreators.undo()
-export const redo = () => UndoActionCreators.redo()
-export const undoAmount = (x) => UndoActionCreators.jumpToPast(x);
--- src/actions/Notifications.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by adam.kazberuk on 12/13/2016.
- */
-export const continueGame = () => ({ type: "CONTINUE_GAME"})
