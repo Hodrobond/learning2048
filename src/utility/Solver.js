6a578ac1085b80c970ecc211980539b7dcf6f6e0
Hodrobond <adamkazb@comcast.net>
2017-01-01 10:30:13 -0800

    let's take a different approach to the solver

--- src/utility/Solver.js
+++ src/utility/Solver.js
@@ -1,26 +1,21 @@
-export const getTileInfo = (board) => {
-//tileQuantity[n] = # of tiles of value 2^n
-//exception: tileQuantity[0] == # of tiles of value 0 (instead of 1)
-  let tileQuantity = [];
-  for(let i=0;i<board.length;i++){
-    for(let j=0;j<board[i].length;j++){
-      let tileValue = board[i][j];
-      let tilePower;
-      if(tileValue > 0)
-        tilePower = Math.log(tileValue)/Math.log(2)
-      else {
-        tilePower = 0;
-      }
-      tileQuantity[tilePower] = tileQuantity[tilePower] ? (tileQuantity[tilePower] + 1) : 1
-    }
+export const rowFilledAmount = (row) => {
+  let count = 0;
+  for(let i=0; i<row.length; i++){
+    if(row[i] !== 0)
+      count++;
   }
-  for(let i=0;i<tileQuantity.length;i++){
-    if(tileQuantity[i] == null)
-      tileQuantity[i] = 0;
-  }
-  return tileQuantity;
+  return count;
 }
 
-export const getHint = (board) => {
-
+export const isRowOrdered = (row) => {
+  //to the right
+  let highestValue = 0;
+  let ordered = true;
+  for(let i=0; i<row.length; i++){
+    if(row[i] < highestValue)
+      ordered = false;
+    if(row[i] > highestValue)
+      highestValue = row[i];
+  }
+  return ordered;
 }
--- src/utility/Solver.test.js
+++ src/utility/Solver.test.js
@@ -1,25 +1,11 @@
 import * as Solver from './Solver'
 
-it('getTileInfo', () => {
-  //value: quantity
-  //0: 3
-  //2: 2
-  //4: 2
-  //8: 2
-  //16: 1
-  //32: 1
-  //64: 3
-  //128
-  //256
-  //512
-  //1024: 1
-  //2048: 1
-  let board = [[0,2,4,8],
-                [2,4,8,32],
-                [16,64,64,64],
-                [2048, 1024,0,0]];
+it('isRowOrdered - true', () => {
+  let row = [0,2,4,8];
+  expect(Solver.isRowOrdered(row)).toEqual(true);
+})
 
-  let solution = [3, 2, 2, 2, 1, 1, 3, 0, 0, 0, 1, 1];
-
-  expect(Solver.getTileInfo(board)).toEqual(solution);
+it('isRowOrdered - false', () => {
+  let row = [0,2,4,2];
+  expect(Solver.isRowOrdered(row)).toEqual(false);
 })
